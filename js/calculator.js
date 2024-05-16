71925e781b92969a605c334f8e6b604a323e34c6
ayomahansani <ayohansi17@gmail.com>
2024-05-16 15:56:17 +0530

    added js for equal button

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,27 @@
 
-let screen = document.getElementsByClassName("screen");
+const inputScreen = document.querySelector(".screen");
 
-let buttons = document.getElementsByClassName("calc-button");
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+buttons.forEach((item) => {
+
+    item.onclick = () => {
+
+        if (item.id === "clear") {
+            inputScreen.innerText = "";
+        } else if (item.id === "backspace") {
+            let string = inputScreen.innerText.toString();
+            inputScreen.innerText = string.substr(0, string.length - 1);
+        } else if (inputScreen.innerText !== "" && item.id === "equal") {
+            inputScreen.innerText = eval(inputScreen.innerText);
+        } else if (inputScreen.innerText === "" && item.id === "equal") {
+            inputScreen.innerText = "Empty!";
+            setTimeout(() => (inputScreen.innerText = ""), 2000);
+        } else {
+            inputScreen.innerText += item.id;
+        }
+
+    };
+
+});
